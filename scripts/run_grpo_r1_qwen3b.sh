71122b074a461542be3eb0e0b4403da193d5577b
huangz <826568389@qq.com>
2025-02-14 18:44:57 +0800

    第一次版本

---
+++ scripts/run_grpo_r1_qwen3b.sh
@@ -0,0 +1,7 @@
+
+
+ACCELERATE_LOG_LEVEL=info accelerate launch \
+--config_file recipes/GRPO3.yaml \
+--num_processes=3 src/grpo_r1/grpo.py \
+--config recipes/GRPO_R1_zero_3B_config.yaml \
+> ./output/grpo_r1_3B_sampling.log 2>&1
